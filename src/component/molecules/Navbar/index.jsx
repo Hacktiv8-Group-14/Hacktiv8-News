3b5620ff73fca212347040ffa2300520ae461304
rereard <rereardany60@gmail.com>
2022-10-13 10:42:13 +0700

    add text-white when dark at navbar

--- src/component/molecules/Navbar/index.jsx
+++ src/component/molecules/Navbar/index.jsx
@@ -14,7 +14,7 @@ export default function Navbar () {
     }
 
     return (
-        <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-white px-2 dark:text-white sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <Logo/>
                 <div className="flex md:order-2">
