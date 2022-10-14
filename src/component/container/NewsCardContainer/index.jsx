0350dc941f3aad478d28beedb6ac7b33d5fb280a
rereard <rereardany60@gmail.com>
2022-10-15 02:10:40 +0700

    add skeleton for news card using react-skeleton

--- src/component/container/NewsCardContainer/index.jsx
+++ src/component/container/NewsCardContainer/index.jsx
@@ -1,7 +1,23 @@
+import NewsCardSkeleton from '../../molecules/NewsCardSkeleton'
+
+import { useSelector } from 'react-redux'
+
 function NewsCardContainer({children}) {
+    
+    const isPending = useSelector((state) => state.news.isPending)
+
     return(
         <div className="mt-4 w-11/12 grid sm:grid-cols-2 md:grid-cols-4 gap-4">
-            {children}
+            {isPending ? (
+                <>
+                <NewsCardSkeleton/>
+                <NewsCardSkeleton/>
+                <NewsCardSkeleton/>
+                <NewsCardSkeleton/>
+                </>
+            ) : (
+                children  
+            )}
         </div>
     )
 }
