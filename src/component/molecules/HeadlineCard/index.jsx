a5a642cef9361aac010e56fc324c68e872327a40
muchlisaryn <muchlisar68@gmail.com>
2022-10-23 11:43:16 +0700

    delete style.css

--- src/component/molecules/HeadlineCard/index.jsx
+++ src/component/molecules/HeadlineCard/index.jsx
@@ -1,5 +1,4 @@
 import Button from "../../atoms/Button"
-import "./style.css"
 import {HiArrowCircleRight, HiArrowCircleLeft} from "react-icons/hi"
 import { useEffect } from "react"
 
@@ -24,8 +23,8 @@ export default function HeadlineCard ({image, title, description, url, index, se
     return (
         <div className="w-11/12 relative text-white">
             <div className="absolute left-0 top-0 rounded-tl-lg rounded-br-lg bg-red p-2">Hot News</div>
-            <Button className={`NavNext absolute inset-y-0 left-0 ml-5`}><HiArrowCircleLeft size={50}  onClick={onClickleft}/></Button>
-            <Button className="NavNext absolute inset-y-0 right-0 mr-5"><HiArrowCircleRight size={50}  onClick={onClickRight}/></Button>
+            <Button className="absolute inset-y-0 left-0 ml-5"><HiArrowCircleLeft size={50}  onClick={onClickleft}/></Button>
+            <Button className="absolute inset-y-0 right-0 mr-5"><HiArrowCircleRight size={50}  onClick={onClickRight}/></Button>
             <img className=" w-full rounded-lg h-96" src={image}alt="headline"/>
             <div className="mx-18 absolute inset-x-0 bottom-0 p-5 backdrop-blur-sm  bg-black bg-opacity-50 rounded-b-lg">
                 <div className="border-b mb-2 font-bold truncate pb-2">{title}</div>
